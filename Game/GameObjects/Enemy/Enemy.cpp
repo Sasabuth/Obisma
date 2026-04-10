3462f67e4cc42e72e7103dfe53e4014a7e37df2b
Sasabuth <it242189@trident.ac.jp>
2026-04-10 18:14:37 +0900

    デバック消し忘れ

--- Game/GameObjects/Enemy/Enemy.cpp
+++ Game/GameObjects/Enemy/Enemy.cpp
@@ -164,7 +164,7 @@ void Enemy::Update(float elapsedTime)
 void Enemy::Render()
 {
 	auto context = m_pUserResources->GetDeviceResources()->GetD3DDeviceContext();
-	auto states = m_pUserResources->GetCommonStates();
+	//auto states = m_pUserResources->GetCommonStates();
 	auto view = m_pUserResources->GetView();
 	auto proj = m_pUserResources->GetProject();
 
